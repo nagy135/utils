0fa3b0faa0be53840aaf931843026b59dfdb0f97
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-04-03 16:24:57 +0200

    feat(primes): dummy iteration method

--- primes/src/main.rs
+++ primes/src/main.rs
@@ -17,7 +17,7 @@ impl Timeguard {
 impl Drop for Timeguard {
     fn drop(&mut self) {
         match self.start.elapsed() {
-            Ok(start) => println!("{}: ,took: {}", self.name, start.as_secs_f64()),
+            Ok(start) => println!("{} took: {}", self.name, start.as_secs_f64()),
             Err(e) => println!("Timeguard Error {:?}", e),
         }
     }
@@ -26,13 +26,27 @@ impl Drop for Timeguard {
 fn main() {
     {
         let _tg = Timeguard::new("dummy".to_string());
-        let dummy = primes::dummy(4);
+        let dummy = primes::dummy(1000);
         println!("Dummy answer: {}", dummy);
     }
 }
 
 mod primes {
-    pub fn dummy(n: usize) -> usize {
-        5
+    pub fn dummy(mut n: usize) -> usize {
+        let mut cur = 2;
+        while n > 0 {
+            let mut is_prime = true;
+            for i in 2..cur {
+                if cur % i == 0 {
+                    is_prime = false;
+                    break;
+                }
+            }
+            if is_prime {
+                n -= 1;
+            }
+            cur += 1;
+        }
+        cur - 1
     }
 }
