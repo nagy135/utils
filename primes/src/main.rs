29ceb01e629d82acec62c67c03f84c605e1eb38a
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-04-03 17:34:07 +0200

    feat(primes): eratosten with reusing Vec

--- primes/src/main.rs
+++ primes/src/main.rs
@@ -31,21 +31,41 @@ fn main() {
     }
     {
         let _tg = Timeguard::new("eratorsten".to_string());
-        let dummy = primes::eratosten(20, 100);
-        println!("Eratorsten answer: {}", dummy);
+        let eratosten = primes::eratosten(1000, 100);
+        println!("Eratorsten answer: {}", eratosten);
     }
 }
 
 mod primes {
     pub fn eratosten(mut n: usize, buffer_size: usize) -> usize {
         let mut buffer: Vec<usize> = Vec::with_capacity(buffer_size);
+        let mut primes: Vec<usize> = Vec::new();
         let mut last_i = 0;
         for i in 0..buffer_size {
             buffer.push(i + 1);
         }
+        let mut last_max = buffer[buffer_size - 1];
         while n > 0 {
             last_i += 1;
+            if last_i >= buffer_size {
+                last_i = 0;
+                let upper_bound = last_max + buffer_size;
+                buffer.clear();
+                for i in last_max..upper_bound {
+                    let i = i + 1;
+                    buffer.push(i);
+                    for p in primes.clone() {
+                        if i % p == 0 {
+                            buffer.pop();
+                            buffer.push(0);
+                        }
+                    }
+                }
+                last_max += buffer_size;
+            }
+            // 0s on strides
             if buffer[last_i] != 0 {
+                primes.push(buffer[last_i]);
                 n -= 1;
                 let mut i = last_i;
                 while (i + buffer[last_i]) < buffer_size {
@@ -54,7 +74,7 @@ mod primes {
                 }
             }
         }
-        buffer[last_i]
+        primes.last().unwrap().clone()
     }
 
     pub fn dummy(mut n: usize) -> usize {
