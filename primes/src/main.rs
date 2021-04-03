be7d0491cbdda05ff10389f84a0dc8dadcacf622
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-04-03 16:50:08 +0200

    feat(primes): eratosten with fixed buffer_size (Vec)

--- primes/src/main.rs
+++ primes/src/main.rs
@@ -29,9 +29,34 @@ fn main() {
         let dummy = primes::dummy(1000);
         println!("Dummy answer: {}", dummy);
     }
+    {
+        let _tg = Timeguard::new("eratorsten".to_string());
+        let dummy = primes::eratosten(20, 100);
+        println!("Eratorsten answer: {}", dummy);
+    }
 }
 
 mod primes {
+    pub fn eratosten(mut n: usize, buffer_size: usize) -> usize {
+        let mut buffer: Vec<usize> = Vec::with_capacity(buffer_size);
+        let mut last_i = 0;
+        for i in 0..buffer_size {
+            buffer.push(i + 1);
+        }
+        while n > 0 {
+            last_i += 1;
+            if buffer[last_i] != 0 {
+                n -= 1;
+                let mut i = last_i;
+                while (i + buffer[last_i]) < buffer_size {
+                    i += buffer[last_i];
+                    buffer[i] = 0;
+                }
+            }
+        }
+        buffer[last_i]
+    }
+
     pub fn dummy(mut n: usize) -> usize {
         let mut cur = 2;
         while n > 0 {
